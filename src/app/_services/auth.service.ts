0d139fbe3373a892cfbf9c803cc22b5a673b6f4a
Olasunkanmi Onibonoje <onibuzz102@gmail.com>
2020-10-12 23:50:45 -0400

    Themes and Alertjs

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { constants } from 'buffer';
 import {map} from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl ='http://localhost:5000/api/auth/'
+  baseUrl ='http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken : any;
 
 constructor(private http: HttpClient) {}
 
@@ -16,7 +19,9 @@ login(model: any){
     map((response: any)=>{
       const user = response;
       if(user){
-           localStorage.setItem('token',user.token)
+           localStorage.setItem('token',user.token);
+           this.decodedToken = this.jwtHelper.decodeToken(user.token);
+           console.log(this.decodedToken);
       }
     })
   );
